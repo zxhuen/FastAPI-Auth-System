f9d7ec54a6a6ec7b7b2d59416ae61982250232b9
zxhuen <zxhuen324@gmail.com>
2026-07-03 19:18:05 +0800

    bug fixes and added decode token

--- app/services/auth_services.py
+++ app/services/auth_services.py
@@ -1,15 +1,33 @@
 from datetime import datetime, timedelta, timezone
 from jose import jwt
 from app.core.config import settings
+from jwt.exceptions import ExpiredSignatureError, InvalidTokenError
+from fastapi import HTTPException
 
 
 
 
 def create_access_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + timedelta(settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.now(timezone.utc) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode["exp"] = expire
 
     token = jwt.encode(to_encode, settings.SECRET, settings.ALGORITHM)
     
     return token
+
+def decode_access_token(token: str):
+
+    try:
+        payload = jwt.decode(token, settings.SECRET, algorithms=[settings.ALGORITHM])
+        return payload
+    except ExpiredSignatureError:
+        raise HTTPException(
+            status_code=401,
+            detail="Access token has expired"
+        )
+    except InvalidTokenError:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid access token"
+        )
