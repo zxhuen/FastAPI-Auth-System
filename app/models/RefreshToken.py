2243eb2c523bbafb8417a2ad2e07319d9b9a445b
zxhuen <zxhuen324@gmail.com>
2026-07-06 13:26:51 +0800

    added refresh token model

--- app/models/RefreshToken.py
+++ app/models/RefreshToken.py
@@ -1,3 +1,42 @@
-from sqlalchemy import Column, Integer, String, DateTime, Boolean, UUID, func
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, UUID, ForeignKey, func
 from app.core.database import Base
 from sqlalchemy.orm import relationship
+import uuid
+from datetime import datetime, timezone
+
+class RefreshToken(Base):
+    __tablename__ = "refresh_tokens"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+
+    user_id = Column(
+        UUID(as_uuid=True),
+        ForeignKey("users.id"),
+        nullable=False
+    )
+    user = relationship("User", back_populates="refresh_tokens")
+
+    jti = Column(
+        UUID(as_uuid=True),
+        unique=True,
+        nullable=False
+    )
+
+    expires_at = Column(
+        DateTime(timezone=True),
+        nullable=False
+    )
+
+    revoked = Column(
+        Boolean,
+        default=False,
+        nullable=False
+    )
+
+    created_at = Column(
+        DateTime(timezone=True),
+        default=lambda: datetime.now(timezone.utc),
+        nullable=False
+    )
+
+    
--- app/models/User.py
+++ app/models/User.py
@@ -12,6 +12,8 @@ class User(Base):
     role_id = Column(Integer, ForeignKey("roles.id"), nullable=False)
     role = relationship("Role", back_populates="users")
 
+    refresh_tokens = relationship("RefreshToken", back_populates="refresh_tokens")
+
     username = Column(String(20), unique=True, nullable=False)
     email = Column(String(255), unique=True, nullable=False, index=True)
 
